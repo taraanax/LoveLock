526e70d6bfebd4c489574a71d802c29c3893e17c
Tara <tara.ana.fosnaric@gmail.com>
2026-01-31 00:56:45 +0100

    frame

---
+++ src/main/java/org/example/LoveLock.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LoveLock {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Love Lock");
+        frame.setSize(420, 260);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        frame.setVisible(true);
+    }
+
+
+}
