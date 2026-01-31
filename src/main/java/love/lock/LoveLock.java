48e61743efbcbdbd098bd345a294a75ffe2eb8c1
Tara <tara.ana.fosnaric@gmail.com>
2026-01-31 01:26:16 +0100

    button pa mogoc poskusi

--- src/main/java/love/lock/LoveLock.java
+++ src/main/java/love/lock/LoveLock.java
@@ -2,8 +2,13 @@ package love.lock;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoveLock {
+
+    static int poskusi = 3;
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Love Lock");
@@ -18,14 +23,37 @@ public class LoveLock {
 
         JPasswordField passwordField = new JPasswordField(15);
         JButton unlockButton = new JButton("Odkleni");
-        
+
+        JLabel message = new JLabel(" ");
+
+        String correctPassword = "21.5.";
+
+        //big boss
+        unlockButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String enteredPassword = new String(passwordField.getPassword());
+
+                if (enteredPassword.equals(correctPassword)) {
+                    title.setText("Odklenjeno ^_^");
+                    message.setText("Pol leta pikica pol leta aah message");
+                    unlockButton.setEnabled(false);
+                    passwordField.setEnabled(false);
+                } else {
+                    poskusi--;
+
+                    //if (poskusi > 0) {
+                        //message.setText(getNamig(poskusi));
+                    //}
+                }
+            }
+        });
 
         frame.add(title);
         frame.add(instruction);
         frame.add(passwordField);
         frame.add(unlockButton);
+        frame.add(message);
         frame.setVisible(true);
     }
-
-
 }
